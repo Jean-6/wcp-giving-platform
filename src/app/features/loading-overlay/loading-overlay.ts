a2164e33ec368ee345119aaa951547c35e5eb4c0
Jean <oka.jeanjaures@gmail.com>
2025-07-03 12:59:01 +0200

    loading covering

---
+++ src/app/features/loading-overlay/loading-overlay.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-loading-overlay',
+  imports: [],
+  templateUrl: './loading-overlay.html',
+  styleUrl: './loading-overlay.css'
+})
+export class LoadingOverlay {
+
+}
