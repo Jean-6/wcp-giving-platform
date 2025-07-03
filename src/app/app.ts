73b3a27302b65ce158c419b1a82d6d1a8a3a06f6
Jean <oka.jeanjaures@gmail.com>
2025-07-03 14:09:18 +0200

    user data form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PaymentForm} from './features/payment-form/payment-form';
+import {NgIf} from '@angular/common';
+import {LoadingOverlay} from './features/loading-overlay/loading-overlay';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaymentForm, NgIf, LoadingOverlay],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App{
   protected title = 'wcp-giving-platform';
+  isImageLoaded = false;
+
+
+  @ViewChild(PaymentForm,{ static: false }) modalChild!: PaymentForm;
+
+  openChild(event: any): void{
+    this.modalChild.openModal();
+  }
+
+  onImageLoad() {
+    //this.isImageLoaded=true;
+  }
 }
