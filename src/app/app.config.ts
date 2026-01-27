531e82f854edf0c3529330f13905fb736296b16e
Jean <oka.jeanjaures@gmail.com>
2026-01-27 21:06:15 +0100

    install and config primeuix to set style of primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import {MessageService} from 'primeng/api';
 import {ToastModule} from 'primeng/toast';
+
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     MessageService,
     ToastModule,
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options:{
+          prefix: 'p',
+          darkModeSelector: 'none'
+        }
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
